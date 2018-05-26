9b3b59cd79a22f3bc0a618538b2a9c048888035b
slewis96 <seblewis96@gmail.com>
2018-05-26 17:47:49 +0100

    finished story needs updated gif

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ function enterCastle(name){
   }
   else{
     alert("please enter a valid input");
-    enterCastle(character);
+    enterCastle(name);
   }
 }
 function roomOption(name){
@@ -133,5 +133,90 @@ function chicken(name){
 function items(name){
   alert(name+" sees a table with three items, these might be helpful later on, there's a large chicken wing, a length of rope, and a hammer.");
   alert("A note warns you only to take two items");
-  var itemlist = prompt("Which items will +"name+" take?", "(rope, hammer), (chicken, rope) etc.");
+  var itemList = prompt("Which items will "+name+" take?", "rope, hammer, chicken (separate by commas)")
+  var itemsArr = itemList.split(',');
+  if(itemsArr.length<=1){
+    alert("Take at least two items");
+    items(name);
+  }
+  itemsArr[0] = itemsArr[0].trim().toLowerCase();
+  itemsArr[1] = itemsArr[1].trim().toLowerCase();
+  if(itemsArr.length>2){
+    document.body.innerHTML = '';
+    document.write (
+    "<body>"+
+    "<img id='frame' src='imgs/fall.gif'>"+
+    "<button id='nextButton' onclick='location.reload()' style='position:absolute; top:0; width: 300px; height: 300px;' type='button' name='button'>Try Again?</button>"+
+    "</body>"+
+    "<script src='js/app.js' 'type='text/javascript'></script>"
+    );
+    alert(name + " ignored the note as they went to pick up the "+itemsArr[2].trim()+" a trap door opened up");
+  }
+  else{
+    if(((itemsArr[0]!="rope")&&(itemsArr[0]!="hammer")&&(itemsArr[0]!="chicken"))||((itemsArr[1]!="rope")&&(itemsArr[1]!="hammer")&&(itemsArr[1]!="chicken"))){
+      alert("please enter a valid input");
+      items(name);
+    }
+    else{
+      document.body.innerHTML = '';
+      document.write (
+      "<body>"+
+      "<img id='frame' src='imgs/skeleton.png'>"+
+      "<button id='nextButton' onclick='boss("+'"'+name+'"'+", "+'"'+itemsArr[0]+'"'+", "+'"'+itemsArr[1]+'"'+")' style='position:absolute; top:0; width: 300px; height: 300px;' type='button' name='button'>Next</button>"+
+      "</body>"+
+      "<script src='js/app.js' 'type='text/javascript'></script>"
+      );
+      alert(name + " picked up the " +itemsArr[0]+" and "+itemsArr[1]+" and moved on");
+    }
+  }
+}
+function boss(name, item1, item2){
+  alert(name + " is confronted by a spooky skeleton he bellows 'who dares enter my castle'");
+  var use = prompt(name + " must think fast " +name+" reaches into their pocket what do they use?", item1 +" or "+item2);
+
+  if(use!=item1&&use!=item2){
+    alert("you don't have that item");
+    boss(name, item1, item2);
+  }
+  else{
+    switch(use){
+      case "chicken":
+        wrongItem(name, use);
+        break;
+      case "rope":
+        wrongItem(name, use);
+        break;
+      case "hammer":
+        rightItem(name);
+        break;
+    }
+  }
+}
+function wrongItem(name, use){
+  if(use=="chicken"){
+    alert(name+ " eats the large chicken wing as the skeleton approaches.... he kills " + name)
+  }
+  if(use=="rope"){
+    alert(name+ " tries to untangle the rope as the skeleton approaches.... he kills " + name)
+  }
+  document.body.innerHTML = '';
+  document.write (
+  "<body>"+
+  "<img id='frame' src='imgs/skeletondead.gif'>"+
+  "<button id='nextButton' onclick='location.reload();' style='position:absolute; top:0; width: 300px; height: 300px;' type='button' name='button'>Try Again?</button>"+
+  "</body>"+
+  "<script src='js/app.js' 'type='text/javascript'></script>"
+  );
+}
+function rightItem(name){
+  alert(name+" hits the skeleton with the hammer, his skull cracks, he drops, "+name+" hits him again, "+name+" can't stop.");
+  alert("only powdered bones remain "+name+" wipes the sweat from their brow you've finished it")
+  document.body.innerHTML = '';
+  document.write (
+  "<body>"+
+  "<img id='frame' src='imgs/theend.gif'>"+
+  "<button id='nextButton' onclick='location.reload();' style='position:absolute; top:0; width: 300px; height: 300px;' type='button' name='button'>Sorry</button>"+
+  "</body>"+
+  "<script src='js/app.js' 'type='text/javascript'></script>"
+  );
 }
